e168de4335b0b26d59fa40f12a1f0b47031835f4
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-05 05:12:39 +0200

    ## Business Layer - Services And Managers Added
    ## 4 Type Of Themes Added

---
+++ EasyCashIdentity.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,51 @@
+﻿using EasyCashIdentity.BusinessLayer.Abstract;
+using EasyCashIdentity.DataAccessLayer.Abstract;
+using EasyCashIdentity.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.BusinessLayer.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal = customerAccountDal;
+        }
+
+        public void TDelete(CustomerAccount t)
+        {
+            //throw new NotImplementedException();
+            _customerAccountDal.Delete(t);
+        }
+
+        public CustomerAccount TGetByID(int id)
+        {
+            //throw new NotImplementedException();
+            return _customerAccountDal.GetByID(id);
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            //throw new NotImplementedException();
+            return _customerAccountDal.GetList();
+        }
+
+        public void TInsert(CustomerAccount t)
+        {
+            //throw new NotImplementedException();
+            _customerAccountDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccount t)
+        {
+            //throw new NotImplementedException();
+            _customerAccountDal.Update(t);
+        }
+    }
+}
