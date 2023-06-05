7a2bf745ae78d81839ba81173b1ec3f976a6cebf
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-05 03:39:28 +0200

    ## Identity Package Added
    ## First Migration
    
    ## Database Reflection Succesful

---
+++ EasyCashIdentity.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EasyCashIdentity.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.DataAccessLayer.Concrete
+{
+    // Identity Context
+    public class Context : IdentityDbContext
+    {
+        // SQL Connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-7U4KRQM;initial catalog=EasyCashDb;integrated security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
