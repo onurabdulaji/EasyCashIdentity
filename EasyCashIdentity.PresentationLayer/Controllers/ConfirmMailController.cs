1abf646b94d30acb992456e50d5c4dbbc88af699
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-06 02:35:23 +0200

    ## Mail Activation Code
    ## Mail Activation Redirection

--- EasyCashIdentity.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentity.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,21 +1,38 @@
-﻿using EasyCashIdentity.PresentationLayer.Models;
+﻿using EasyCashIdentity.EntityLayer.Concrete;
+using EasyCashIdentity.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentity.PresentationLayer.Controllers
 {
     public class ConfirmMailController : Controller
     {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
-        public IActionResult Index(int id)
+        public IActionResult Index()
         {
             var value = TempData["Mail"];
-            ViewBag.v = value + " A A A ";
+            ViewBag.v = value;
             return View();
         }
         [HttpPost]
-        public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+        public async Task <IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
         {
+            var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                return RedirectToAction("Index" , "MyProfile");
+            }
             return View();
         }
     }
 }
+
+// kullanici adi = sifre eslesmedi <-> email confirmed olmadi
