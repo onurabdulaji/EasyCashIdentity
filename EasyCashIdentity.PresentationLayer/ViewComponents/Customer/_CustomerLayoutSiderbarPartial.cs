9f37ca247587c48fc1adcad547f33c752242ea55
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-06 18:07:32 +0200

    ## Customer UI Layout Added

---
+++ EasyCashIdentity.PresentationLayer/ViewComponents/Customer/_CustomerLayoutSiderbarPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentity.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutSiderbarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
